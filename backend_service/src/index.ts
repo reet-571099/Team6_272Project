2dd965f8befc7528403c4fa913aff3cd732f9348
rajat343 <rajatmishra343@gmail.com>
2024-11-26 19:15:44 -0800

    cors fix backend

--- backend_service/src/index.ts
+++ backend_service/src/index.ts
@@ -9,7 +9,12 @@ import UploadRoutes from "./upload/upload.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+);
 app.use(cookieParser());
 app.use(passport.initialize());
 
