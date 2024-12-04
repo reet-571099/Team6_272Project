516575aacc7818bd61b67a510573bfd384516877
rajat343 <rajatmishra343@gmail.com>
2024-12-04 14:44:18 +0530

    port corrected

--- backend_service/src/index.ts
+++ backend_service/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 // app.use(cors());
 
 const corsOptions = {
-	origin: "http://localhost:3000", // Allow only your frontend origin
+	origin: "http://localhost:3001", // Allow only your frontend origin
 	credentials: true, // Allow cookies and authentication headers
 };
 app.use(cors(corsOptions));
